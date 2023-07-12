741cd67bf064fc9074390d12f1e9a0434eb505a7
beratridvanasilturk <br.asltrk@gmail.com>
2023-07-12 16:26:53 +0300

    Created Basic Table View 100 Cell

--- YouHaveToDo/YouHaveToDo/YouHaveToDoViewController.swift
+++ YouHaveToDo/YouHaveToDo/YouHaveToDoViewController.swift
@@ -5,28 +5,43 @@
 //  Created by Berat Rıdvan Asiltürk on 11.07.2023.
 //
 
+// FIXME: Some todo list issues should be fix
 import UIKit
 
 class YouHaveToDoViewController: UITableViewController {
-    //MARK: Variables
-
-    //MARK: Outlets
+    //MARK: - Variables
     
-    //MARK: Functions
+    //MARK: - Outlets
+    
+    //MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 5
+    
+    // MARK: - Table View Data Source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "YouHaveToDoItem")
-        return cell!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "YouHaveToDoItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "1. Row"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "2. Row"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "3. Row"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "4. Row"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "5. Row"
+        }
+        
+        return cell
+        //MARK: - Actions
     }
-
-    //MARK: Actions
 }
 
