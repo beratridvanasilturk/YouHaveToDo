3a72cbc30c10d5fd8e0385979230a96b44bf043a
beratridvanasilturk <br.asltrk@gmail.com>
2023-07-27 14:29:17 +0300

    Added All Lists Scene Functionality for Add New Checklist or Delete It

--- YouHaveToDo/YouHaveToDo/AllListsViewController.swift
+++ YouHaveToDo/YouHaveToDo/AllListsViewController.swift
@@ -14,7 +14,30 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController {
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
+    
+    // MARK: - Stubs
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist) {
+        let newRowIndex = lists.count
+        lists.append(checklist)
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist) {
+        if let index = lists.firstIndex(of: checklist) {
+            let indexPath = IndexPath(row: index, section: 0)
+            if let cell = tableView.cellForRow(at: indexPath) {
+                cell.textLabel!.text = checklist.name
+            }
+        }
+    }
     
     // MARK: - Constants
     // Yeni bir table view cell elde etmek için AllListsViewController'da bu islemler (alternatif yol ile) tamamen kod ile olusturulur. Bunun ilk ayagi let cellIdentifier ile baslar. viewDidLoad icerisindeki tableView.register ile devam eder ve cellForRowAt icerisindeki let cell = tableView.dequeueReusableCell ile 3 adimda yeni bir table view cell olusumu tamamlanir. YouHaveToDoViewController'da bunu tek bir adimda (dequeueReusableCell withIdentifier ile tamamlamistik. dequeueReusableCell çağrısı hala burada da mevcut, ancak daha önce storyboard'un bu tanımlayıcıya sahip bir prototip hücresi vardı ama AllListViewController'da yok.)
@@ -71,13 +94,23 @@ class AllListsViewController: UITableViewController {
     // prepare funtion'u bir view controller'dan segue gerçekleşmeden hemen önce çağrılan hazirlik durumunda gerceklesir.  Burada, görünür hale gelmeden önce yeni view controller'in özelliklerini ayarlama şansımiz olur.
     // prepare(for:sender:) içinde, YouHaveToDoViewController'a kullanıcının dokunduğu satırdaki Checklist nesnesini geçirmemiz gerekir. Bu nesneyi daha önce sender parametresine koymamızın sebebi budur. (didselectrowat'da)
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         if segue.identifier == "ShowYouHaveToDo" {
             // Note: Type cast, Swift'e bir değeri farklı bir veri türüne sahipmiş gibi yorumlamasını söyler.
             //  as!: değeri belirttiğiniz türde olmaya zorlar, type cast'ın başarısız olma olasılığı yoktur. Bu nedenle, YuHaveToDoViewController'a özgü özelliklerden herhangi birine erişmeden önce onu genel olan UIViewController türünden bu uygulamada kullanılan özel türe (YouHaveToDoViewController) dönüştürmeniz gerekir.
             
             let controller = segue.destination as! YouHaveToDoViewController
             controller.checklist = sender as? Checklist
+            
+        } else if segue.identifier == "AddChecklist" {
+            let controller = segue.destination as! ListDetailViewController
+            controller.delegate = self
         }
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        lists.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
 }
